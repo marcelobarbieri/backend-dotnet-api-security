c370266aa722527397de8e25b6da38da1758a27a
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-12-03 10:34:29 -0300

    [JWT e Bearer na Prática] Iniciando o Token Service

--- JwtAspNet/Services/TokenService.cs
+++ JwtAspNet/Services/TokenService.cs
@@ -1,9 +1,14 @@
-﻿namespace JwtAspNet.Services;
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace JwtAspNet.Services;
 
 public class TokenService
 {
     public string Create()
     {
+        var handler = new JwtSecurityTokenHandler();
 
+        var token = handler.CreateToken();
+        return handler.WriteToken(token);
     }
 }
