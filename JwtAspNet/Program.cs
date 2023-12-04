324d26228fb492d9d1cd7114d9824edb5081d563
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-12-04 08:48:22 -0300

    [JWT e Bearer na Prática] Claims Identity Extension

--- JwtAspNet/Program.cs
+++ JwtAspNet/Program.cs
@@ -1,4 +1,5 @@
 using JwtAspNet;
+using JwtAspNet.Extensions;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -58,11 +59,11 @@ app
         handler: (ClaimsPrincipal user) =>
             new
             {
-                id = user.Claims.FirstOrDefault(x=> x.Type == "id").Value,
-                name = user.Claims.FirstOrDefault(x=> x.Type == ClaimTypes.Name).Value,
-                email = user.Claims.FirstOrDefault(x=> x.Type == ClaimTypes.Email).Value,
-                givenName = user.Claims.FirstOrDefault(x=> x.Type == ClaimTypes.GivenName).Value,
-                image = user.Claims.FirstOrDefault(x=> x.Type == "image").Value
+                id = user.Id(),
+                name = user.Name(),
+                email = user.Email(),
+                givenName = user.GivenName(),
+                image = user.Image()
             })
     .RequireAuthorization();
 
