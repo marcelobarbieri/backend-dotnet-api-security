626492d3d39ecb87847db96464bc48f90a5eb9bd
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-12-07 08:38:07 -0300

    [Criando um Sistema de Login] Gerando senhas aleatórias

--- JwtStore/JwtStore.Core/AccountContext/ValueObjects/Password.cs
+++ JwtStore/JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -1,4 +1,5 @@
 ﻿using JwtStore.Core.SharedContext.ValueObjects;
+using System.Security.Cryptography;
 
 namespace JwtStore.Core.AccountContext.ValueObjects;
 
@@ -9,4 +10,44 @@ public class Password : ValueObject
 
     public string Hash { get; } = String.Empty;
     public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+
+    private static string Generate(
+        short length = 16,
+        bool includeSpecialChars = true,
+        bool upperCase = false)
+    {
+        var chars = includeSpecialChars ? (Valid + Special) : Valid;
+        var startRandom = upperCase ? 26 : 0;
+        var index = 0;
+        var res = new char[length];
+        var rnd = new Random();
+
+        while (index < length)
+            res[index++] = chars[rnd.Next(startRandom, chars.Length)];
+
+        return new string(res);
+    }
+
+    private static string Hashing(
+        string password,
+        short saltSize = 16,
+        short keySize = 32,
+        int iterations = 10000,
+        char splitChar = '.')
+    {
+        if (string.IsNullOrEmpty(password))
+            throw new Exception("Password should not be null or empty");
+
+        password += Configuration.Secrets.PasswordSaltKey;
+
+        using var algorithm = new Rfc2898DeriveBytes(
+            password,
+            saltSize,
+            iterations,
+            HashAlgorithmName.SHA256);
+        var key = Convert.ToBase64String(algorithm.GetBytes(keySize));
+        var salt = Convert.ToBase64String(algorithm.Salt);
+
+        return $"{iterations}{splitChar}{salt}{splitChar}{key}";
+    }
 }
