9036ecd60cba740377bb1bc49995005de6e48447
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-12-09 19:51:35 -0300

    [Criando um Sistema de Login] Password Hashing

---
+++ JwtStore/JwtStore.Core/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.NetworkInformation;
+
+namespace JwtStore.Core;
+
+public static class Configuration
+{
+    public static SecretsConfiguration Secrets { get; set; } = new();
+
+    public class SecretsConfiguration
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string JwtPrivateKey { get; set; } = string.Empty;
+        public string PasswordSaltKey { get; set; } = string.Empty;
+    }
+}
+ 
