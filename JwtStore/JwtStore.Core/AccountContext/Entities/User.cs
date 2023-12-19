66a86ceaf7862d0a342eb4b5bce612a36eb6d36c
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2023-12-19 07:25:21 -0300

    [Criando um Sistema de Login] Mapeando o User

--- JwtStore/JwtStore.Core/AccountContext/Entities/User.cs
+++ JwtStore/JwtStore.Core/AccountContext/Entities/User.cs
@@ -16,7 +16,7 @@ public class User : Entity
         Password = new Password(password);
     }
 
-    public string Nome { get; private set; } = string.Empty;
+    public string Name { get; private set; } = string.Empty;
     public Email Email { get; private set; } = null!;
     public Password Password { get; private set; } = null!;
     public string Image { get; private set; } = string.Empty;
