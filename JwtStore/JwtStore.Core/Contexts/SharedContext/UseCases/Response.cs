64be4be9dcab7b8e8f2a19bfe7b53ab7d79db4a2
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2024-02-26 08:41:34 -0300

    [Criando um Sistema de Login] Response

---
+++ JwtStore/JwtStore.Core/Contexts/SharedContext/UseCases/Response.cs
@@ -0,0 +1,12 @@
+﻿using Flunt.Notifications;
+
+namespace JwtStore.Core.Contexts.SharedContext.UseCases;
+
+public abstract class Response
+{
+    public string Message { get; set; } = string.Empty;
+    public int Status { get; set; } = 400;
+    public IEnumerable<Notification>? Notifications { get; set; }
+
+    public bool IsSucess => Status is >= 200 and <= 299;
+}
