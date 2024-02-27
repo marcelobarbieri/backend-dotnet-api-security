3bab8c8733497f678560ad7198a08e602837bbfb
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2024-02-27 08:33:16 -0300

    [Criando um Sistema de Login] Compondo a Resposta

--- JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
+++ JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flunt.Notifications;
 
-namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+
+public class Response : SharedContext.UseCases.Response
 {
-    internal class Response
+    protected Response()
     {
+
     }
+
+    public Response(
+        string message, 
+        int status, 
+        IEnumerable<Notification>? notifications = null)
+    {
+        Message = message;
+        Status = status;
+        Notifications = notifications;
+    }
+
+    public Response(
+        string message, 
+        ResponseData data)
+    {
+        Message = message;
+        Status = 201;
+        Notifications = null;
+        Data = data;
+    }
+
+    public ResponseData? Data { get; set; }
 }
+
+public record ResponseData(Guid Id, string Name, string Email);
