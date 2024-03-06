419518bb6aac4f9effb5e0fcb1607da950c42b24
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2024-03-06 16:02:58 -0300

    [Criando um Sistema de Login] Implementando o serviço

--- JwtStore/JwtStore.Core/Configuration.cs
+++ JwtStore/JwtStore.Core/Configuration.cs
@@ -5,6 +5,8 @@ namespace JwtStore.Core;
 public static class Configuration
 {
     public static SecretsConfiguration Secrets { get; set; } = new();
+    public static EmailConfiguration Email { get; set; } = new();
+    public static SendGridConfiguration SendGrid { get; set; } = new();
     public static DatabaseConfiguration Database { get; set; } = new();
 
     public class SecretsConfiguration
@@ -18,5 +20,15 @@ public static class Configuration
     {
         public string ConnectionString { get; set; } = string.Empty;
     }
+
+    public class EmailConfiguration
+    {
+        public string DefaultFromEmail { get; set; } = "test@barbieri.dev.br";
+        public string DefaultFromName { get; set; } = "barbieri.dev.br";
+    }
+
+    public class SendGridConfiguration
+    {
+        public string ApiKey { get; set; } = string.Empty;
+    }
 }
- 
