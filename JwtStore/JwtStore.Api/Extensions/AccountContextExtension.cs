e322047a4de534717314ffde370e8aec296089b4
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2024-03-06 21:12:33 -0300

    [Criando um Sistema de Login] Registrando as dependências

--- JwtStore/JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore/JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -4,7 +4,17 @@ public static class AccountContextExtension
 {
     public static void AddAccountContext(this WebApplicationBuilder builder)
     {
+        #region Create
 
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository, // Interface
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Repository>(); // Implementação
+
+        builder.Services.AddTransient<
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IService, // Interface
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>(); // Implementação
+
+        #endregion
     }
 
     public static void MapAccountEndpoints(this WebApplication app)
