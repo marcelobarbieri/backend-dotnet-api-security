42e5a6f1e8cfd42d6e1de290f054b9e7f1916c81
Marcelo de Paiva Barbieri <marcelodpbarbieri@gmail.com>
2024-03-08 21:53:09 -0300

    [Criando um Sistema de Login] Adicionando Roles ao Token

--- JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Handler.cs
+++ JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Handler.cs
@@ -74,7 +74,7 @@ public class Handler : IRequestHandler<Request, Response>
                 Id = user.Id.ToString(),
                 Name = user.Name,
                 Email = user.Email,
-                Roles = Array.Empty<string>()
+                Roles = user.Roles.Select(x => x.Name).ToArray()
             };
 
             return new Response(string.Empty, data);
